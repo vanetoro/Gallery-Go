19962f0dbe55b2fc45f587cfd89333f65432833c
Vanessa Toro <vanetoro84@gmail.com>
2018-11-10 16:43:32 -0500

    applied content theme

--- src/components/galleries.js
+++ src/components/galleries.js
@@ -4,11 +4,19 @@ import Gallery from './gallery'
 function Galleries(props) {
 
     return (
-        <div>
-        <ul>
-            {props.events.map(event => <li><Gallery event={event}/></li>)}
-        </ul>
+    
+        <div className="container py-4 py-lg-5">
+        {/* <!-- News --> */}
+            <h3 className="text-uppercase text-letter-spacing-sm my-0 text-primary font-weight-bold">
+                Most Popular 
+            </h3>
+            <hr className="mt-2 mb-3" />
+            <div className="row">
+            
+                    {props.events.map(event => <Gallery event={event}/>)}
         </div>
+    </div>   
+        
     )
 }
 
