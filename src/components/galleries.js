9899730543b0d37f79ec58938391597b4e2df32f
Vanessa Toro <vanetoro84@gmail.com>
2018-11-27 17:40:59 -0500

    columns displaying properly

--- src/components/galleries.js
+++ src/components/galleries.js
@@ -3,8 +3,8 @@ import Gallery from './gallery'
 
 function Galleries(props) {
 
+    // debugger
     return (
-    
         <div className="container py-4 py-lg-5">
         {/* <!-- News --> */}
             <h3 className="text-uppercase text-letter-spacing-sm my-0 text-primary font-weight-bold">
@@ -12,8 +12,10 @@ function Galleries(props) {
             </h3>
             <hr className="mt-2 mb-3" />
             <div className="row">
-            
-                    {props.events.map(event => <Gallery event={event}/>)}
+            <div className="card-columns">
+
+                    {props.events.map(event =>  <Gallery event={event}/> )}
+            </div>
         </div>
     </div>   
         
