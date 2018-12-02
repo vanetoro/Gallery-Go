3b439adc42ecb568ddc499c22bc7af3662a21c1d
Vanessa Toro <vanetoro84@gmail.com>
2018-12-02 15:36:04 -0500

    displays name and descrip

--- src/components/exhibit.js
+++ src/components/exhibit.js
@@ -1,6 +1,21 @@
-import React, {Component} from 'React'
+import React from 'react'
+// import { connect } from 'react-redux'
+import { withRouter} from "react-router-dom";
 
-class Exhibit extends Component{
+function Exhibit(props){
+    // set event based on id found at the end of the url
+    let event = props.events[parseInt(props.match.params.name.split('-').slice(-1)[0]) -1]
 
-    
+    return(
+
+        <div>
+            <img src={event.imgUrl}/>
+            <h1>{event.eventName}</h1>
+            {event.eventDescription}
+        
+        
+        </div>
+    )
 }
+
+export default withRouter(Exhibit)
