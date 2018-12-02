c8f450dda5dd3be1a1bb7a24c9cc9cfe11f0a7da
Vanessa Toro <vanetoro84@gmail.com>
2018-12-02 15:39:24 -0500

    set fn for empty props handling

--- src/components/galleries.js
+++ src/components/galleries.js
@@ -2,19 +2,22 @@ import React from 'react'
 import Gallery from './gallery'
 
 function Galleries(props) {
-
-    // debugger
+   
+   const exhibits = () => { if(props.events === undefined){
+        return <div>Loading..</div>
+    } else{ 
+        return props.events.map(event =>  <Gallery event={event}/> )
+    }}
     return (
         <div className="container py-4 py-lg-5">
-        {/* <!-- News --> */}
             <h3 className="text-uppercase text-letter-spacing-sm my-0 text-primary font-weight-bold">
                 Most Popular 
             </h3>
             <hr className="mt-2 mb-3" />
             <div className="row">
             <div className="card-columns">
-
-                    {props.events.map(event =>  <Gallery event={event}/> )}
+                    {exhibits()}
+                    
             </div>
         </div>
     </div>   
