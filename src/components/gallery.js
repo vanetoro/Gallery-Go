982c0e38a0a7a074a1773bf2df4dbf3981d5f5ce
Vanessa Toro <vanetoro84@gmail.com>
2018-12-02 15:40:02 -0500

    set link to show page

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,26 +1,24 @@
 import React, {Component} from 'react'
 import '../App.css';
-// import { Switch } from 'react-router-dom'
-var classNames = require('classnames');
+import { Link} from 'react-router-dom';
+import { stringToSlug } from '../actions/helper';
+// var classNames = require('classnames');
 
 function Gallery(props) {
 
-    const handleClick = (e) => {
-      e.preventDefault()
-    }
-
         return (
             <>
-            {/* <Link to="" */}
-            <div className="card mb-4 border-0 flex-ew"  data-id={props.event.id} onClick={handleClick}>
-                  <img className="card-img img-fluid" data-src={props.event.imageUrl} data-toggle="blazy" />
-                  <div className="card-img-overlay bg-dark bg-op-5 text-white flex-valign-b" >
-                      <h5 className="card-title mb-1" >
-                        <div className='title'>{props.event.eventName}</div>
-                      </h5>
-                      <p className="card-text text-uppercase text-white text-sm">{props.event.venueName}</p>
-                  </div>    
-            </div>
+            <Link to={`/exhibits/${stringToSlug(props.event.eventName)}-${props.event.id}`}>
+                <div className="card mb-4 border-0 flex-ew"   data-id={props.event.id}>
+                    <img className="card-img img-fluid" data-src={props.event.imageUrl} data-toggle="blazy" />
+                    <div className="card-img-overlay bg-dark bg-op-5 text-white flex-valign-b" >
+                        <h5 className="card-title mb-1" >
+                            <div className='title'>{props.event.eventName}</div>
+                        </h5>
+                        <p className="card-text text-uppercase text-white text-sm">{props.event.venueName}</p>
+                    </div>    
+                </div>
+            </Link>
             </>
         )
 }
