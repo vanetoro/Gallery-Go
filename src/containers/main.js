a8f5e5a680d7d0db60c0ca011ea88abffbe5f9e4
Vanessa Toro <vanetoro84@gmail.com>
2018-12-02 23:04:09 -0500

    setup routing & localStorage

--- src/containers/main.js
+++ src/containers/main.js
@@ -1,53 +1,69 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import $ from "jquery";
 import Galleries from '../components/galleries'
+import Exhibit from '../components/exhibit'
+import {BrowserRouter as Router,Route} from 'react-router-dom'
+
 
 class Main extends Component {
-     
+
     state = {
         events: []
     }
 
-    componentDidMount(){
+    componentDidMount() {
         //need to figure out cors errors
-        $.ajax( {
-            url: 'http://www.nyartbeat.com/list/event_mostpopular.en.xml',
-            type: 'GET',
-            dataType: "text xml"
-        })
-        .then(xmlData => {
-            let events = xmlData.getElementsByTagName('Event')
-            let eventsObj = []
-            for(var i = 0; i< events.length; i++){
-                // debugger
-                eventsObj.push({
-                    id: i,
-                    eventName: events[i].getElementsByTagName('Name')[0].textContent,
-                    eventDescription: events[i].getElementsByTagName('Description')[0].textContent,
-                    venueName: events[i].getElementsByTagName('Venue')[0].getElementsByTagName('Name')[0].textContent,
-                    venueAddress: events[i].getElementsByTagName('Venue')[0].getElementsByTagName('Address')[0].textContent,
-                    DateStart: events[i].getElementsByTagName('DateStart')[0].textContent,
-                    DateEnd: events[i].getElementsByTagName('DateEnd')[0].textContent,
-                    imageUrl: events[i].getElementsByTagName('Image')[2].outerHTML.match(/\bhttp\S+/)[0].slice(0,-1)
-
-
+        $.get({
+                url: 'http://www.nyartbeat.com/list/event_mostpopular.en.xml',
+                dataType: "text xml"
             })
-            }
-            this.setState({events: eventsObj})
-        });
-        }
-    
+            .done(xmlData => {
+                let events = xmlData.getElementsByTagName('Event')
+                let eventsArray = []
+                for (var i = 1; i < events.length; i++) {
+                    eventsArray.push({
+                        id: i,
+                        eventName: events[i].getElementsByTagName('Name')[0].textContent,
+                        eventDescription: events[i].getElementsByTagName('Description')[0].textContent,
+                        venueName: events[i].getElementsByTagName('Venue')[0].getElementsByTagName('Name')[0].textContent,
+                        venueAddress: events[i].getElementsByTagName('Venue')[0].getElementsByTagName('Address')[0].textContent,
+                        DateStart: events[i].getElementsByTagName('DateStart')[0].textContent,
+                        DateEnd: events[i].getElementsByTagName('DateEnd')[0].textContent,
+                        imageUrl: events[i].getElementsByTagName('Image')[2].outerHTML.match(/\bhttp\S+/)[0].slice(0, -1)
+                        
+                    })
+                    
+                }
+                this.setState({
+                    events: eventsArray
+                })
+                // this.populateLocalStorage()
+                localStorage.setItem(i, JSON.stringify(eventsArray[i -1]));
+            });
+    }
+
+    // populateLocalStorage(){
+    //     for(let i = 1; i<this.state.events; i++){
+    //         localStorage.setItem(i, JSON.stringify(this.state.events[i]));
+    //     }
+    // }
+
+   
+
     render() {
-        // debugger
         return (
-            <Switch>
-                <Route exact path="/exhibits/:id" component={exhibits} />
-            </Switch>
-            <div id="content" className="p-0 bg-white">
-                <Galleries events={this.state.events}/>
-            </div>
-        )
+            <>
+                <Router>
+                    <div>
+                        <div id="content" className="p-0 bg-white">
+                            <Route exact path="/" render={ ()=> < Galleries events={ this.state.events } />}/> 
+                        </div> 
+                        <Route path="/exhibits/:name" render={ ()=> <Exhibit events={ this.state.events } />}/>
+                    </div> 
+                </Router> 
+            </>
+            )
     }
 }
 
-export default Main
+            export default Main
